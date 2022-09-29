e2598f4ea71d79cdba74b5fee852fb09bf0a15c5
tan <121097539@qq.com>
2022-09-29 17:37:15 +0800

    1

--- sbStart/src/test/java/org/example/AppTest.java
+++ sbStart/src/test/java/org/example/AppTest.java
@@ -2,7 +2,11 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.example.pojo.PeopleForm;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -16,5 +20,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+
+    public static void main(String[] args) {
+        PeopleForm p1 = new PeopleForm();
+        PeopleForm p2 = new PeopleForm();
+        List<PeopleForm> list = new ArrayList<>();
+        list.add(p1);
+        list.add(p2);
+        list.stream().forEach(a->a.setAge(1));
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getAge());
+        }
     }
 }
