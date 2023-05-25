799dedc3b569e87e4d3dffbb33c8bc340591061d
tan <121097539@qq.com>
2023-05-25 09:30:30 +0800

    test fastjson case sensitivity

--- test/src/test/java/com/tan/test/JsonHandler.java
+++ test/src/test/java/com/tan/test/JsonHandler.java
@@ -4,6 +4,9 @@ import com.alibaba.fastjson.JSON;
 import com.tan.test.model.Person;
 import com.tan.test.model.Person2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonHandler {
 
     public static void main(String[] args) {
@@ -24,5 +27,12 @@ public class JsonHandler {
 //        p2.setAGe(11);
         jsonString = JSON.toJSONString(p2);
         System.out.println(jsonString);
+
+        List<Person> list = new ArrayList<>();
+        list.add(p1);
+        String jsonString1 = JSON.toJSONString(list);
+        List<Person2> person2s = JSON.parseArray(jsonString1, Person2.class);
+        System.out.println("person2s.get(0).getNAme()");
+        System.out.println(person2s.get(0).getNAme());
     }
 }
